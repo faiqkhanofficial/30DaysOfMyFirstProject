589bea3ce187229c79c582eb97c1c4c3aca2818f
faiqkhanofficial <faiqkhanofficial786@gmail.com>
2022-07-16 22:10:03 +0500

    Day @ completed

---
+++ lib/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  var n = 1;
+  var pi = 3.146;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Center(child: (Text("Catalog App")))),
+        drawer: Drawer(),
+        body: Center(
+            child: Container(child: Text("My First $n App as pi = $pi"))));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,15 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Material(
-            child: Container(child: Center(child: Text("My First App")))));
+    return MaterialApp(home: HomePage());
   }
 }
